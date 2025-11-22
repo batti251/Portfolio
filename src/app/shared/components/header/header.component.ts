66da3c9e0505348d6a0dbd1c2a71f8d883ebbb1f
batti251 <bastibuenz25@gmail.com>
2025-11-22 23:56:55 +0100

    feat: adjust hero- and menu-overlay basic - styling

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -14,33 +14,54 @@ import { inject } from '@angular/core';
 })
 
 
-export class HeaderComponent{
-  activeLanguage:string = "en";
+export class HeaderComponent {
+  activeLanguage: string = "en";
   menuActive = false;
   animationReady = false;
   router = inject(Router);
 
   /**
    * This Function sets the current language, for either german or english Version
-   * @param language - choice between 
+   * @param language - choice between English (EN) and German (DE) Version
    */
-  setActiveStatus(language:string){
+  setActiveStatus(language: string) {
     this.activeLanguage = language
   }
 
-  navigateToMenu(){
-     this.animationReady = true;
+  /**
+   * This Function handles the burger-menu navigation
+   */
+  navigateToMenu() {
+    this.animationReady = true;
     if (this.router.url != '/menu') {
-        this.menuActive = true
-      setTimeout(() => {
-        this.router.navigate(['/menu'])
-      }, 800);
+      this.navigateToMenupage();
     } else {
-        this.menuActive = false
-      setTimeout(() => {
-        this.router.navigate([''])
-      }, 800);
+      this.navigateToMainpage()
     }
   }
-}
 
+  /**
+   * This Function navigates to the Menu page
+   * It sets a 800ms timer, before navigation
+   * It sets menuActive to true, for burger-animation (burger -> X)
+   */
+  navigateToMenupage() {
+    this.menuActive = true
+    setTimeout(() => {
+      this.router.navigate(['/menu'])
+    }, 800);
+  }
+
+  /**
+   * This Function navigates back to the Main page
+   * It sets a 800ms timer, before navigation
+   * It sets menuActive to false, for burger-animation (X -> burger)
+   */
+  navigateToMainpage() {
+    this.menuActive = false
+    setTimeout(() => {
+      this.router.navigate([''])
+    }, 800);
+  }
+
+}
