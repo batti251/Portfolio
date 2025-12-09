abce19c6f6813e2e9b50b1e02844aa349fef5ade
batti251 <bastibuenz25@gmail.com>
2025-12-09 20:02:50 +0100

    feat[references]: add reference component design and responsive

--- src/app/main/references/references.component.ts
+++ src/app/main/references/references.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from "@ngx-translate/core";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe, CommonModule],
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss'
 })
 export class ReferencesComponent {
+
+
+
+  references = [
+    {
+      name: "felix",
+      relation: "Team Partner"
+    },
+    {
+      name: "ozan",
+      relation: "Team Partner"
+    }
+  ]
 
 }
