0b0eff6d260320342263d278828110033c5f574d
batti251 <bastibuenz25@gmail.com>
2025-12-19 21:08:27 +0100

    feat [privacy]: add privacy component and responsive design

--- src/app/privacy/privacy.component.ts
+++ src/app/privacy/privacy.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './privacy.component.html',
   styleUrl: './privacy.component.scss'
 })
 export class PrivacyComponent {
 
+   arr = [
+    {data1: 'inventory data'},
+      {data2: 'contact data'},
+      {data3: 'content data'},
+      {data4: 'usage data'},
+      {data5: 'meta'},
+      {data6: 'log data'}
+    
+  ]
+
+  affectedPersons = [
+    {data1: 'communication partner'},
+    {data2: 'user'}
+  ]
+
+
+  purpose = [
+     {data1: 'communication'},
+      {data2: 'security measures'},
+      {data3: 'organizational and administrative procedures'},
+      {data4: 'feedback'},
+      {data5: 'Provision of our online services and user-friendliness'},
+      {data6: 'IT Infrastructure'},
+      {data7: 'public relations'}
+  ]
+
+  legalRelevance = [0,1,2,3]
 }
