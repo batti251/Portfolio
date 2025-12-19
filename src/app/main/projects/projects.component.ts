b2b804ce7fb385d5b8b8a07f017ea4884f08ee74
batti251 <bastibuenz25@gmail.com>
2025-12-19 18:02:28 +0100

    fix [footer]: change ngClass query to have proper form visibility

--- src/app/main/projects/projects.component.ts
+++ src/app/main/projects/projects.component.ts
@@ -11,53 +11,54 @@ import { TranslatePipe } from "@ngx-translate/core";
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent implements OnInit,AfterViewInit {
+export class ProjectsComponent implements OnInit, AfterViewInit {
   @ViewChildren('elements', { read: ElementRef })
   elements!: QueryList<ElementRef<HTMLElement>>;
   videoUrls: SafeResourceUrl[] = [];
   public screenWidth: any;
-private observer!: IntersectionObserver;
- constructor(private sanitizer: DomSanitizer) {
-  this.videoUrls = this.projects.map(p =>
-    this.sanitizer.bypassSecurityTrustResourceUrl(p.url)
-  );
-}
+  private observer!: IntersectionObserver;
 
+  constructor(private sanitizer: DomSanitizer) {
+    this.videoUrls = this.projects.map(p =>
+      this.sanitizer.bypassSecurityTrustResourceUrl(p.url)
+    );
+  }
 
-ngAfterViewInit(): void {
-this.observeElements();
-}
+  ngAfterViewInit(): void {
+    this.observeElements();
+  }
 
-ngOnDestroy(): void {
-  this.observer?.disconnect();
-}
+  ngOnDestroy(): void {
+    this.observer?.disconnect();
+  }
 
-ngOnInit() {
-      this.screenWidth = window.innerWidth;
-}
- @HostListener('window:resize')
+  ngOnInit() {
+    this.screenWidth = window.innerWidth;
+  }
+  @HostListener('window:resize')
   onResize() {
     this.screenWidth = window.innerWidth;
   }
 
 
-/**
- * This Function sets up an IntersectionObserver to toggle in-view-class 
- * It's triggered when elements reach 40% visibility.
- * 
- */
-observeElements(){
- this.observer = new IntersectionObserver(
-    (entries) => {
-      for (let entry of entries) {
-        let element = entry.target as HTMLElement;
-        let inView = entry.intersectionRatio >= 0.4;
-        element.classList.toggle('in-view', inView);
-      }},
-    {threshold: [0, 0.4, 1]}
-  );
-  this.elements.forEach(element => this.observer.observe(element.nativeElement));
-}
+  /**
+   * This Function sets up an IntersectionObserver to toggle in-view-class 
+   * It's triggered when elements reach 40% visibility.
+   * 
+   */
+  observeElements() {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        for (let entry of entries) {
+          let element = entry.target as HTMLElement;
+          let inView = entry.intersectionRatio >= 0.4;
+          element.classList.toggle('in-view', inView);
+        }
+      },
+      { threshold: [0, 0.4, 1] }
+    );
+    this.elements.forEach(element => this.observer.observe(element.nativeElement));
+  }
 
 
   projects = [{
