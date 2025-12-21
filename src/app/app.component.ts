5ead98dfa661747dbb99c4524ccf20bb56de6617
batti251 <bastibuenz25@gmail.com>
2025-12-21 14:13:09 +0100

    fix [style]: align imprint and privacy sections width and max-wwidth according to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { FooterComponent } from "./shared/components/footer/footer.component";
+
 @Component({
   selector: 'app-root',
   standalone: true,
