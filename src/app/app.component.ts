0c0291d43634a9cb8ef87e7de92ea26c9f148300
batti251 <bastibuenz25@gmail.com>
2025-12-21 21:49:35 +0100

    fix [anchoring]: adjust anchoring and style elements for stable scrollToHash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, AfterViewInit, ApplicationRef, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./shared/components/header/header.component";
-import { FooterComponent } from "./shared/components/footer/footer.component";
+import { ViewportScroller } from '@angular/common';
+import { filter, take } from 'rxjs';
+
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements AfterViewInit {
+  private appRef = inject(ApplicationRef);
+  private scroller = inject(ViewportScroller);
+
+  ngAfterViewInit() {
+    this.appRef.isStable
+      .pipe(filter(Boolean), take(1))
+      .subscribe(() => this.scrollToHash());
+  }
+
+  private scrollToHash() {
+    const id = location.hash.slice(1);
+    if (!id) return;
+
+    this.scroller.setOffset([0, 88]);
+    requestAnimationFrame(() => this.scroller.scrollToAnchor(id));
+  }
 }
