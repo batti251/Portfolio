8c92a8aac4aae8eab6d1ec405a562a850a1d9c29
batti251 <bastibuenz25@gmail.com>
2026-08-02 11:40:51 +0200

    build(version): update to Angular Material 20

--- src/app/main/arrow/arrow.component.ts
+++ src/app/main/arrow/arrow.component.ts
@@ -1,9 +1,8 @@
 import { Component, Input} from '@angular/core';
-import { NgOptimizedImage } from "@angular/common";
 
 @Component({
     selector: 'app-arrow',
-    imports: [NgOptimizedImage],
+    imports: [],
     templateUrl: './arrow.component.html',
     styleUrl: './arrow.component.scss'
 })
